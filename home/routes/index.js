91a6b225c22af3772bf8138448e755daa9b07e0d
Sigvart M. Hovland <sigvart.m@gmail.com>
2015-02-17 16:00:18 +0100

    Forgot the routing

--- home/routes/index.js
+++ home/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sigvart M. Hovland', description: '"Embedded system programmer, Webenthusiast, live for the IoT and occationally funny"' });
+  res.render('index', { title: 'Sigvart M. Hovland', description: '"Embedded system programmer, web enthusiast, live for the IoT and occationally funny"' });
+});
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Blog'});
+});
+router.get('/is/professional', function(req, res, next) {
+  res.render('cv', { title: 'CV'});
 });
 
 module.exports = router;
